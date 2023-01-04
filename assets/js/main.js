0800daacac92b97bc930a3e0b58f44729c8eec84
LandoADeveloper <landom2222@gmail.com>
2023-01-04 13:23:20 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+let info = document.getElementById("info")
+
+info.innerHTML = "<h1>Hello World</h1>"
+info.innerHTML += "<h2>How are you?</h2>"
+
+
+let container = document.getElementById("container")
+
+container.innerHTML = "<p>start of element</p>"
+document.write("end of element")
